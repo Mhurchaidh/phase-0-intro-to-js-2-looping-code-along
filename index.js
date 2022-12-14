95b24e7c7d70d96d34b9d8e94032fa0c8d36a105
mhurchaidh <bmurchie1997@gmail.com>
2022-12-13 16:01:15 -0800

    commit tree 24bdcc115d232f1d03fe74cbd45c03c5102f798f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names = [], eventName){
+    const thankYouNotes = [];
+
+    for (let i = 0; i < names.length; i++){
+        thankYouNotes.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouNotes;
+}
+
+function countDown(i){
+    while (i >= 0){
+        console.log(i)
+        i--;
+    }
+}
+
+
